e05c5b81c4cfdd6942bc43de180ef639e4bcc675
mthaugusto <mthaugusto94@icloud.com>
2023-05-11 15:55:04 -0300

    PSET0 - Exercises

---
+++ Problem Set 0/einstein.py
@@ -0,0 +1,8 @@
+
+# In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
+
+mass = int(input("Insert mass as an integer (in kilograms)"))
+
+jaules = mass * (300000000*300000000)
+
+print(jaules)
