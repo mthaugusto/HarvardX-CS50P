a5c88ed6bf78bcad1b51578efe61472671fac1a9
Matheus Leite <126774875+mthaugusto@users.noreply.github.com>
2023-05-25 23:49:42 -0300

    PSET2 - Exercises

--- Problem Set 2/plates.py
+++ Problem Set 2/plates.py
@@ -6,6 +6,13 @@
 # “No periods, spaces, or punctuation marks are allowed.”
 # In plates.py, implement a program that prompts the user for a vanity plate and then output Valid if meets all of the requirements or Invalid if it does not. Assume that any letters in the user’s input will be uppercase. Structure your program per the below, wherein is_valid returns True if s meets all requirements and False if it does not. Assume that s will be a str. You’re welcome to implement additional functions for is_valid to call (e.g., one function per requirement).
 
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
 def is_valid(s):
 
     if len(s) < 2 or len(s) > 6:
@@ -30,4 +37,5 @@ def is_valid(s):
 
     else:
         return True
+
 main()
